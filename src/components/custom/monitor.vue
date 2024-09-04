19734b0eefc337ca19fedd97036ed4cfced1e102
Ferinco <emmanuelferrum003@gmail.com>
2024-09-04 02:34:38 +0100

    returned the preloader

--- src/components/custom/monitor.vue
+++ src/components/custom/monitor.vue
@@ -3,13 +3,13 @@
         <div class="hidden md:flex justify-center items-center gap-2 flex-col w-12 h-full">
             <div v-for="(dot, index) in 4" :key="index"
                 class="w-6 h-6 rounded-full border-2 border-black flex justify-center items-center">
-                <div class="w-[83%] h-[83%] mt-[1px] rounded-full" :class="getClassName(index)">
+                <div class="w-[83%] h-[83%] mt-[1px] rounded-full transition-all duration-100" :class="getClassName(index)">
                 </div>
             </div>
-            <h1 id="h1">
+            <!-- <h1 id="h1">
                 {{ scrollY }}
 
-            </h1>
+            </h1> -->
         </div>
     </div>
 </template>
@@ -33,8 +33,8 @@ export default {
             } else if (scrollY >= 5360) {
                 active.value = 3;
             }
-            document.getElementById("h1").innerText = scrollY
-        console.log(scrollY)
+        //     document.getElementById("h1").innerText = scrollY
+        // console.log(scrollY)
 
         }
 
